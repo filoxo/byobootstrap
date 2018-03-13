495268f78989454cac17cd31e30112a875663c70
Carlos Filoteo <c.filoteo@live.com>
2018-03-12 23:42:42 -0600

    Add Intro

---
+++ src/slides/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Slide, Heading, Text } from 'spectacle'
+
+export default (
+  <Slide transition={['fade']}>
+    <Heading size={2} lineHeight={1.3}>
+      Build your own Bootstrap
+    </Heading>
+    <Text margin="10px 0 0" size={2} textColor="tertiary">
+      Create your own CSS framework
+    </Text>
+  </Slide>
+)
