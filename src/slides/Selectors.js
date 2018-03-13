afc2d5a0f3f1263e335b64e914aea7f773a65251
Carlos Filoteo <c.filoteo@live.com>
2018-03-13 00:14:23 -0600

    Add Selectors

---
+++ src/slides/Selectors.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Slide, Heading, Text } from 'spectacle'
+import SimpleTerminal from '../SimpleTerminal'
+
+export default (
+  <Slide transition={['fade']}>
+    <SimpleTerminal>
+        Test test test
+    </SimpleTerminal>
+  </Slide>
+)
