e765e5178e30d7464d4c590564549e76d25307fe
Carlos Filoteo <carlosfiloteo@canopytax.com>
2018-04-15 00:11:38 -0600

    Add SpecificityQuiz1 to slides

--- src/slides/index.js
+++ src/slides/index.js
@@ -1,11 +1,13 @@
 import Intro from './Intro'
 import WhyBuild from './WhyBuild'
 import Selectors from './Selectors'
+import SpecificityQuiz1 from './SpecificityQuiz.1' 
 
 const Slides = [
     Intro,
     WhyBuild,
-    Selectors
+    Selectors,
+    SpecificityQuiz1
 ]
 
 export default Slides
