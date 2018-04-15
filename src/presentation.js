c6faa1cb7b5e425fe1bd7b2734ae3d1d9dd383a7
Carlos Filoteo <carlosfiloteo@canopytax.com>
2018-04-14 22:42:47 -0600

    Format

--- src/presentation.js
+++ src/presentation.js
@@ -7,19 +7,16 @@ import { Deck } from 'spectacle';
 import Slides from './slides'
 
 // Import theme
-import createTheme from 'spectacle-theme-nova';
-import 'spectacle-theme-nova/syntax/prism.nova.css';
+import createTheme from 'spectacle-theme-nova'
+import 'spectacle-theme-nova/syntax/prism.nova.css'
 
 // Require CSS
 require('normalize.css');
 
-const theme = createTheme(
-  null,
-  {
-    primary: 'Montserrat',
-    secondary: 'Helvetica',
-  }
-);
+const theme = createTheme(null, {
+  primary: 'Montserrat',
+  secondary: 'Helvetica'
+})
 
 export default class Presentation extends React.Component {
   render() {
