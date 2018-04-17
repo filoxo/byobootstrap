e835d6224140e8229b132fb302890a9fd3edf29c
Carlos Filoteo <carlosfiloteo@canopytax.com>
2018-04-16 22:36:11 -0600

    Add GetToWork slide

---
+++ src/slides/GetToWork.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Slide, Heading, Text } from 'spectacle'
+
+export default (
+  <Slide transition={['fade']}>
+    <Heading size={2} lineHeight={1.3}>
+      Let's get to work!
+    </Heading>
+    <Text margin="10px 0 0" style={{ fontSize: '3rem' }}>
+      codepen.io/filoxo/project/editor/ZBBqyj
+    </Text>
+  </Slide>
+)
--- src/slides/index.js
+++ src/slides/index.js
@@ -4,6 +4,7 @@ import Selectors from './Selectors'
 import SpecificityQuiz1 from './SpecificityQuiz.1' 
 import SpecificityQuiz2 from './SpecificityQuiz.2'
 import SpecificityQuiz3 from './SpecificityQuiz.3' 
+import GetToWork from './GetToWork'
 
 const Slides = [
   Intro,
@@ -11,7 +12,8 @@ const Slides = [
   Selectors,
   SpecificityQuiz1,
   SpecificityQuiz2,
-  SpecificityQuiz3
+  SpecificityQuiz3,
+  GetToWork
 ]
 
 export default Slides
