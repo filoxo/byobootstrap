7715476c0ec73c1db7f64c326c283d8ce14cbd6c
Carlos Filoteo <carlosfiloteo@canopytax.com>
2018-04-17 15:51:59 -0600

    Move selectors slide to later

--- src/slides/index.js
+++ src/slides/index.js
@@ -1,9 +1,9 @@
 import Intro from './Intro'
 import WhyBuild from './WhyBuild'
-import Selectors from './Selectors'
 import SpecificityQuiz1 from './SpecificityQuiz.1' 
 import SpecificityQuiz2 from './SpecificityQuiz.2'
 import SpecificityQuiz3 from './SpecificityQuiz.3' 
+import Selectors from './Selectors'
 import GetToWork from './GetToWork'
 import Typography from './Typography'
 import Buttons from './Buttons'
@@ -13,10 +13,10 @@ import Grid from './Grid'
 const Slides = [
   Intro,
   WhyBuild,
-  Selectors,
   SpecificityQuiz1,
   SpecificityQuiz2,
   SpecificityQuiz3,
+  Selectors,
   GetToWork,
   Typography,
   Buttons,
