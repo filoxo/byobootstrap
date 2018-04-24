7a536c96c75996fffa12a9d3e7e046aeeda14bfe
Carlos Filoteo <carlosfiloteo@canopytax.com>
2018-04-24 16:36:52 -0600

    Improve Specificity questions

--- src/slides/SpecificityQuiz.2.js
+++ src/slides/SpecificityQuiz.2.js
@@ -1,5 +1,19 @@
 import React from 'react'
-import { Slide, Heading, Appear, CodePane } from 'spectacle'
+import { Slide, Heading, Appear, CodePane, Code, Text } from 'spectacle'
+import { selector } from '../styles'
+
+const quizCode = `<style>
+#pageTitle {
+  background-color: lightcoral;
+}
+h1 {
+  background-color: lightblue;
+}
+.title {
+  background-color: lightgreen;
+}
+</style>
+<h1 id="pageTitle" class="title">Hello, world!</h1>`
 
 export default (
   <Slide transition={['fade']}>
@@ -7,15 +21,15 @@ export default (
       Specificity Quiz: #2
     </Heading>
     <Heading size={4} lineHeight={1.5}>
-      What is the weight of this selector?
+      What background color will the <Code>h1</Code> have?
     </Heading>
-    <CodePane
-      style={{ fontSize: '3rem' }}
-      lang="css"
-      source={`main#app .container { ... }`}
-    />
-    <Appear>
-      <div style={{ fontSize: '5rem' }}>111</div>
-    </Appear>
+    <div style={selector}>
+      <div style={{ fontSize: '1.25rem', margin: '0 auto' }}>
+        <CodePane lang="html" source={quizCode} />
+      </div>
+      <Appear>
+        <Text style={{ fontSize: '5rem', color: 'lightblue' }}>lightblue</Text>
+      </Appear>
+    </div>
   </Slide>
 )
--- src/slides/SpecificityQuiz.3.js
+++ src/slides/SpecificityQuiz.3.js
@@ -1,20 +1,27 @@
 import React from 'react'
-import { Slide, Heading, Appear, CodePane, Text } from 'spectacle'
+import { Slide, Heading, Appear, CodePane, Code, Text } from 'spectacle'
 import { selector } from '../styles'
 
 const quizCss = `<style>
-h3  {
+.content > p  {
   color: lightcoral;
 }
-
-h3 {
-  color: lightblue;
-}
-
-h2 { 
-  color: lightgreen;
-}
 </style>
+<main>
+  <p>1</p>
+  <section class="content">
+    <p>2</p>
+  </section>
+  <section>
+    <p>3</p>
+  </section>
+  <footer class="content">
+    <p>4</p>
+    <div id="legal">
+      <p>5</p>
+    </div>
+  </footer>
+</main>
 `
 
 export default (
@@ -23,14 +30,23 @@ export default (
       Specificity Quiz: #3
     </Heading>
     <Heading size={4} lineHeight={1.5}>
-      {`Given this CSS, what color will the <h3>s become?`}
+      Which <Code>p</Code> elements will be lightcoral?
     </Heading>
     <div style={selector}>
-      <div style={{ fontSize: '1.25rem', maxWidth: 275, margin: '0 auto' }}>
+      <div style={{ fontSize: '1rem', maxWidth: 400, margin: '0 auto' }}>
         <CodePane lang="html" source={quizCss} />
       </div>
       <Appear>
-        <Text style={{ fontSize: '5rem', color: 'lightblue' }}>lightblue</Text>
+        <Text
+          style={{
+            fontSize: '3rem',
+            fontWeight: 'bold',
+            color: 'lightcoral',
+            marginBottom: 0
+          }}
+        >
+          2 and 4
+        </Text>
       </Appear>
     </div>
   </Slide>
