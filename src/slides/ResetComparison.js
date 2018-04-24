c19151c5ffed9255e69942be3d968c80f9ce1546
Carlos Filoteo <carlosfiloteo@canopytax.com>
2018-04-24 16:36:42 -0600

    Add title

--- src/slides/ResetComparison.js
+++ src/slides/ResetComparison.js
@@ -3,6 +3,9 @@ import { Slide, Text, List, ListItem, Appear, Heading } from 'spectacle'
 
 export default (
   <Slide>
+    <Heading size={2} lineHeight={1.3}>
+      CSS Reset
+    </Heading>
     <Text>
       <List>
         <ListItem>
@@ -14,7 +17,7 @@ export default (
         </ListItem>
         <ListItem>IE uses a different box-sizing model</ListItem>
       </List>
-      <Heading size={3}>CSS Resets provide a consistent baseline</Heading>
+      <Heading size={3}>Provide a consistent baseline</Heading>
     </Text>
   </Slide>
 )
